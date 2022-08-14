25a454bbfed03dbe289a023981a7a4b708c83636
KYapp <kukiyomenaisapp@gmail.com>
2022-08-14 21:55:12 +0900

    Init

---
+++ CodingSoldier/Server.cs
@@ -0,0 +1,22 @@
+﻿using CodingGameBase;
+
+namespace CodingSoldierServer
+{
+    public class Server
+    {
+        public static Action Update;
+        public static CodingGameServer cgserver;
+
+        public static void StartServer(Action update)
+        {
+            Update += update;
+
+            cgserver = new CodingGameServer();
+            cgserver.Tick = () =>
+            {
+                Update();
+            };
+            cgserver.StartServer();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using CodingSoldierServer;
+
+namespace Test
+{
+    public class Program
+    {
+        public int i;
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Server.StartServer(program.Update);
+
+            program.Start();
+            while (true) { }
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("start");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("update" + i);
+
+            i++;
+        }
+    }
+}
